477e8bf26637d20dc6150d646bbb77d27db62836
spike <bank.tellr@gmail.com>
2022-08-04 04:02:32 +0900

    Adds stubs for `suicide?` and `do-captures`

--- src/clj/server/game/core.clj
+++ src/clj/server/game/core.clj
@@ -2,23 +2,38 @@
   (:require [server.game.matrix :as mat]))
 
 
+(defn- next-team [team]
+  (condp = team
+    :black :white
+    :white :black))
+
+(defn- suicide? [mat coord team]
+  :TODO)
+
+(defn- do-captures
+  "Do captures on the given team's stones.
+  If team is :black and their stones are surrounded, return new matrix with those :black stones removed."
+  [mat coord team]
+  :TODO)
+
+(defn- can-place? [mat coord team]
+  "Conditions for placement:
+  1. elem_xy = :empty
+  2.1.1 if (not (suicide? mat [x y] team)) ...
+  2.1.2 recursive search from [x y], search for :empty
+  2.2 Do captures"
+  (let [elem (mat/get-elem mat coord)]
+    (if (or (not= elem :empty)
+            (suicide? mat coord team))
+      false
+      (let [team' (next-team team)]
+        (do-captures mat coord team')))))
+
+
 (defprotocol IGame
   (get-team [this])
   (ended? [this])
   (place [this coord]))
-
-(defn- next-team [team]
-  (condp = team
-    :black :white
-    :white :black
-    nil))
-
-(defn- can-place? [mat [x y] team]
-  ; 1. elem_xy = :empty
-  ; 2.1 Do captures
-  ; 2.2.1 if (not (suicide? mat [x y] team)) ...
-  ; 2.2.2 Recursive search from [x y], search for :empty
-  )
 
 (defrecord Game [dim history]
   IGame
@@ -41,7 +56,7 @@
   (place [this coord]
     (let [{history :history, dim :dim} this
           top   (or (take-last 1 history)
-                    (mat/new-matrix dim))
+                    (mat/new-matrix dim :empty))
           team' (next-team (get-team this))]
       (when (can-place? top coord team')
         (mat/set-elem top coord team')))))
