fcba6a6edec2f5405e21ddaa52f4e49fb5d90869
shoegaze <bank.tellr@gmail.com>
2022-08-05 07:52:46 +0900

    Implements cell style

--- src/cljs/client/cell.cljs
+++ src/cljs/client/cell.cljs
@@ -1,6 +1,32 @@
 (ns client.cell)
 
 
-(defn cell-component [game [x y]]
+(defn- cell-img-class [[w h] [x y]]
+  (let [left?   (= x 0)
+        right?  (= x (- w 1))
+        top?    (= y 0)
+        bottom? (= y (- h 1))
+        corner? (or (and left? top?)
+                    (and left? bottom?)
+                    (and right? top?)
+                    (and right? bottom?))
+        edge?   (and (not corner?)
+                     (or left? right? top? bottom?))
+        inner?  (and (not corner?)
+                     (not edge?))]
+    [(when left?   "left")
+     (when right?  "right")
+     (when top?    "top")
+     (when bottom? "bottom")
+     (when corner? "corner")
+     (when edge?   "edge")
+     (when inner?  "inner")]))
+
+(defn- cell-img [game coord]
+  (let [dim   (:dim @game)
+        class (cell-img-class dim coord)]
+    [:img {:class class}]))
+
+(defn cell-component [game coord]
   [:span.cell
-   [:img {:src "image/board_inner__50_50.png"}]])
+   [cell-img game coord]])
